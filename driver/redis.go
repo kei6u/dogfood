cbc1b29a7b9edb53da3c569b497820fa7b1c865a
kei6u <keisuke.umegaki.630@gmail.com>
2021-11-23 15:25:03 +0900

    set redis password

--- driver/redis.go
+++ driver/redis.go
@@ -13,17 +13,22 @@ import (
 func NewRedis(ctx context.Context) (*redis.Client, func() error, error) {
 	var host string
 	var addr string
+	var pw string
 
 	if host = os.Getenv("REDIS_HOST"); host == "" {
-		return nil, nil, errors.New("host is missing")
+		return nil, nil, errors.New("redis host is missing")
 	}
 	if addr = os.Getenv("REDIS_ADDR"); addr == "" {
-		return nil, nil, errors.New("addr is missing")
+		return nil, nil, errors.New("redis addr is missing")
+	}
+	if pw = os.Getenv("REDIS_PASSWORD"); pw == "" {
+		return nil, nil, errors.New("redis password is missing")
 	}
 
 	c := redis.NewClient(&redis.Options{
-		Addr: fmt.Sprintf("%s:%s", host, addr),
-		DB:   0,
+		Addr:     fmt.Sprintf("%s:%s", host, addr),
+		Password: pw,
+		DB:       0,
 	})
 	return c, c.Close, nil
 }
